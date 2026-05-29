28161fbebf9b9cd99fdeeee9be99539e7184699f
szymon-bosiak <szymon.bosiak.99@gmail.com>
2026-05-29 22:40:05 +0200

    feat: fix routing issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { copyFileSync } from 'fs'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
@@ -14,11 +12,5 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
-    {
-      name: 'copy-index-to-404',
-      closeBundle() {
-        copyFileSync(resolve('dist/index.html'), resolve('dist/404.html'))
-      },
-    },
   ],
 })
